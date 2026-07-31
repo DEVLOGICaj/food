047a717c6c16852c0f8106c122277fc7b04ae2fc
DEVLOGICaj <abdalraheemjniad@gmail.com>
2026-07-31 03:27:13 +0300

    Update food.js

--- food.js
+++ food.js
@@ -3,4 +3,5 @@ const food = [
 {name: "بطاطا",price: "150.00 $",image: ""},
 {name: "كريسبي",price: "400.00 $",image: ""},
 {name: "فتتشيني",price: "450.00 $",image: ""},
+{name: "برغر",price: "250.00 $",image: ""},
 ];
